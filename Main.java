6c9439f2f4aa9930bc1fcb4df2a922869f287af1
PJAvinash <avinashindian2.0@gmail.com>
2023-05-13 14:25:47 -0500

    commit-0

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String filePath = args[0];
         int root = Integer.parseInt(args[1]);
-        Node processingNode = ReadConfig.read(filePath,root);
+        Node processingNode = ReadConfig.read(filePath,184);
         processingNode.startLayeredBFS();     
     }
 }
